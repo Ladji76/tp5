c3d330c9359190da33a28c2f75f1e07c687a05d1
Ladji Dianissy <ladji.dianissy@gmail.com>
2024-04-09 13:20:20 +0200

    ajout

---
+++ README.md
@@ -0,0 +1,6 @@
+**Lebreton :**
+**B**
+**1ere annee**
+**IUT Le Havre - Cours GIT**
+
+ “Nous avons maintenant créé une nouvelle branche de test”
---
+++ src/CryptoMarche.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+
+public class CryptoMarche {
+
+	private ArrayList<Portefeuille> portefeuilles;
+	private static CryptoMarche marche;
+
+	private CryptoMarche() {
+		portefeuilles = new ArrayList<Portefeuille>();
+	}
+
+	public static CryptoMarche getInstance() {
+		if (marche == null) {
+			marche = new CryptoMarche();
+		}
+		return marche;
+	}
+
+	public void ajouter(Portefeuille p) {
+		portefeuilles.add(p);
+	}
+
+	/**
+	 * Cette fonction recherche sur le marché tous les portefeuilles
+	 * du propriétaire et calcule son capital en euros.
+	 * 
+	 * @param proprietaire
+	 * @return capital en euros du proprietaire.
+	 */
+	public double capitalEnEuros(String proprietaire)
+    {
+        double montant=0.0;
+        for(Portefeuille p : portefeuilles)
+        {
+            if(p.getProprietaire() == proprietaire)
+                montant += p.valeurEnEuros();
+        }
+        return montant;
+    }
+
+	/**
+	 * Cette fonction recherche sur le marché tous les portefeuilles
+	 * d'un type de devise et calcule le volume total de capital de
+	 * cette devise sur le marché
+	 * 
+	 * @param monnaie
+	 * @return capital total en circulation de la cryptomonnaie (en euros).
+	 */
+	public double capitalMonneaie(Cryptomonnaie monnaie)
+    {
+        double montant=0.0;
+        for(Portefeuille p : portefeuilles)
+        {
+            if(monnaie == p.getMonnaie())
+            {
+                montant += p.valeurEnEuros();
+            }
+        }
+
+        return montant;
+
+    }
+
+	@Override
+	public String toString() {
+		String ret = "";
+		for (Portefeuille p : this.portefeuilles) {
+			ret += p.toString() + "\n";
+		}
+		return ret;
+	}
+
+}
